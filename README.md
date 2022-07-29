6ea311edb40f606ff2a48377063846287bbfe74c
João <joaovictorpfr@gmail.com>
2022-07-29 15:26:35 -0300

    Add codes

---
+++ README.md
@@ -0,0 +1,8 @@
+# Stopwatch ⏰
+
+**Este projeto de cronômetro, construído no JS foi um dos projetos em que aprendi muito sobre tempo e como ele deve ser desenvolvido para conseguir desenvolver com que ele mude a forma que ele mostra HTML diretamente, simultâneo com que conta os segundos!**
+
+## Resultado:
+
+<img src="./assets/img/StopwatchPrint.png">
+
---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+window.onload = function () {
+  let = seconds = 00;
+  let appendSeconds = document.getElementById("seconds");
+  let tens = 00;
+  let appendTens = document.getElementById("tens");
+
+  let btnStart = document.getElementById("start");
+  let btnStop = document.getElementById("stop");
+  let btnReset = document.getElementById("reset");
+
+  let interval;
+
+  btnStart.onclick = () => {
+    clearInterval(interval);
+    interval = setInterval(startTimer, 10);
+  };
+  btnStop.onclick = () => {
+    clearInterval(interval);
+  };
+  btnReset.onclick = () => {
+    clearInterval(interval);
+    tens = "00";
+    seconds = "00";
+
+    appendSeconds.innerHTML = seconds;
+    appendTens.innerHTML = tens;
+  };
+  function startTimer(){
+    tens++;
+    
+    if(tens <= 9){
+      appendTens.innerHTML = "0" + tens;
+    };
+    if(tens > 9){
+      appendTens.innerHTML = tens;
+    };
+    if(tens > 99){
+      console.log("seconds");
+      seconds++;
+      appendSeconds.innerHTML = "0" + seconds;
+      tens = 0;
+      appendTens.innerHTML = "0" + 0;
+    };
+    if(seconds > 9){
+      appendSeconds.innerHTML = seconds;
+    }
+  };
+};
